57ec7bb23468244edadcd2d8e867f85f46f29683
Петър Попов <popovstz@gmail.com>
2023-12-10 18:35:05 +0200

    removed unnecessary items

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ from gui.window import MainWindow
 from src.auth import gmail_authenticate
 from src.email_handler import get_user_email
 
+
 def main() -> None:
 
     # run the main frame window
     root = MainWindow()
     root.mainloop()
 
-    # service = gmail_authenticate()
-    # emails = list_emails(service)
-    # store_mail_count(service, emails)
-    # batch_delete(service, "email_sender_data.json")
-    # print(get_user_email(service))
 
 if __name__ == "__main__":
     main()
