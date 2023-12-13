9a85716264e0edcd065f1b65c92a7a5f0226f819
pepk0 <popovstz@gmail.com>
2023-12-13 21:11:44 +0200

    added some docstrings and ggoogle auth impovment

--- src/auth.py
+++ src/auth.py
@@ -1,42 +1,41 @@
-import os
-import pickle
+import os.path
 
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-
-# Request all access 
+# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://mail.google.com/']
-PERSONAL_EMAIL = 'popovstz@gmail.com'
 
 
-def gmail_authenticate() -> object:
-    """ Authenticates the user, so he can use the Gmail API
-    Args:
-        None
-    Returns:
-        GoogleClient object: object which calls the Gmail API service 
-    """
-    creds = None
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
-            creds = pickle.load(token)
+def gmail_authenticate():
+  """Shows basic usage of the Gmail API.
+  Lists the user's Gmail labels.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    # Call the Gmail API
+    return build("gmail", "v1", credentials=creds)  
+  except HttpError:
+    return False
     
-    # if there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        
-        # save the credentials for the next run
-        with open("token.pickle", "wb") as token:
-            pickle.dump(creds, token)
-    try:
-        return build('gmail', 'v1', credentials=creds)
-    except HttpError:
-        return False
